79adaa2a46df168827b7b102a45b0406621f7dfa
Jackie <jackie.chang.coding@gmail.com>
2025-07-14 14:19:59 +0200

    simplified test_chat_interface.py and removed unnecessary imports.

--- aafactory/src/tests/test_chat/test_chat_interface.py
+++ aafactory/src/tests/test_chat/test_chat_interface.py
@@ -1,9 +1,6 @@
 # Basic Initialization Test
 import pytest
-from aafactory.chat.interface import create_chat_interface
-from tests.test_configuration import TEST_AVATAR_IMAGE_PATH
-from PIL import Image
-
+from aafactory.chat.interface import create_chat_interface#
 
 def test_chat_interface_initialization():
     #Test that ChatInterface can be initialized."""
